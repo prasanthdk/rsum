cc00c6d95fae610667180b7943309d0d90d4bb2e
prasanth <prasanth.s@pickzy.com>
2018-07-24 16:32:56 +0530

    first

---
+++ pdfCloud/resources/views/welcome.blade.php
@@ -0,0 +1,251 @@
+@extends('layout.frontend')
+
+@section('content')
+
+<main id="main">
+    <!--==========================
+      Slider Start
+    ============================-->
+    <section id="about" class="">
+        <div class="container about">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 content text-center">
+                    <div class="content-inner wow bounceInLeft">
+                        <h2 class="text-uppercase">WELCOME TO PDF CLOUD</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                        </p>
+                        <a href="javascript:void(0)"  data-toggle="modal" data-target="#myModal" class="btn btn-primary">Upload Document</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <!--==========================
+      Slider End
+    ============================-->
+    <!--==========================
+      Create Resume
+    ============================-->
+    <!-- <section class="create-resume-cols wow fadeInUp">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-sm-8 text-center wow fadeInUp">
+                  <h4>Get Started On Your Resume Now</h4>
+                </div>
+                <div class="col-md-4 col-sm-4 wow fadeInUp text-center">
+                    <a href="select_template.html" class="btn btn-primary">Create your resume</a>
+                </div>
+            </div>
+        </div>
+    </section> -->
+    <!--==========================
+      Create Resume
+    ============================-->
+    <!--============================
+    Service List
+    ============================-->
+    <section class="Service-col">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-3 wow bounceInUp">
+                    <div class="circle-img"  data-toggle="modal" data-target="#myModal">
+                        <div class="circle-content text-center">
+                            <img src="images/pngpdf.png" class="img-responsive"/>
+                            <h6>Convert Png to Pdf</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-3 wow bounceInUp">
+                    <div class="circle-img" data-toggle="modal" data-target="#myModal">
+                        <div class="circle-content text-center">
+                            <img src="images/wordpdf.png" class="img-responsive"/>
+                            <h6>Convert Word to PDF </h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-3 wow bounceInUp">
+                    <div class="circle-img"  data-toggle="modal" data-target="#myModal">
+                        <div class="circle-content text-center">
+                            <img src="images/pdf.png" class="img-responsive"/>
+                            <h6>PDF to Word</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-3 wow bounceInUp">
+                    <div class="circle-img"  data-toggle="modal" data-target="#myModal">
+                        <div class="circle-content text-center">
+                            <img src="images/jpgpdf.png" class="img-responsive"/>
+                            <h6>jPG to PDF</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br><br>
+            <div class="row">
+                <div class="col-md-3 col-sm-3 wow bounceInUp">
+                    <div class="circle-img"  data-toggle="modal" data-target="#myModal">
+                        <div class="circle-content text-center">
+                            <img src="images/textpdf.png" class="img-responsive"/>
+                            <h6>Compress PDF</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-3 wow bounceInUp">
+                    <div class="circle-img"  data-toggle="modal" data-target="#myModal">
+                        <div class="circle-content text-center">
+                            <img src="images/pdf1.png" class="img-responsive"/>
+                            <h6>Compress PDF</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-3 wow bounceInUp">
+                    <div class="circle-img"  data-toggle="modal" data-target="#myModal">
+                        <div class="circle-content text-center">
+                            <img src="images/roatepdf.png" class="img-responsive"/>
+                            <h6>Rotate PDF</h6>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-3 wow bounceInUp">
+                    <div class="circle-img"  data-toggle="modal" data-target="#myModal">
+                        <div class="circle-content text-center">
+                            <img src="images/editpdf.png" class="img-responsive"/>
+                            <h6>Edit PDF</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br><br>
+            <div class="row">
+                <div class="col-md-3 col-sm-3 wow bounceInUp">
+                    <div class="circle-img"  data-toggle="modal" data-target="#myModal">
+                        <div class="circle-content text-center">
+                            <img src="images/form.png" class="img-responsive"/>
+                            <h6>Add fillabble
+                                form</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-3 wow bounceInUp">
+                    <div class="circle-img"  data-toggle="modal" data-target="#myModal">
+                        <div class="circle-content text-center">
+                            <img src="images/signpdf.png" class="img-responsive"/>
+                            <h6>Sign PDF</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-3 wow bounceInUp">
+                    <div class="circle-img"  data-toggle="modal" data-target="#myModal">
+                        <div class="circle-content text-center">
+                            <img src="images/watermark.png" class="img-responsive"/>
+                            <h6>Watermark
+                                to pdf</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-3 wow bounceInUp">
+                    <div class="circle-img"  data-toggle="modal" data-target="#myModal">
+                        <div class="circle-content text-center">
+                            <img src="images/reader.png" class="img-responsive"/>
+                            <h6>Highlight pdf</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <!-- ==========================
+    Info
+   ============================-->
+    <section class="info-cols">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 text-center wow bounceInLeft">
+
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--==========================
+      Info
+    ============================
+    <!--============================
+    Service List
+    ============================-->
+
+    <section class="process-col convert">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-md-4 col-sm-4 wow bounceInUp">
+                    <img src="images/hand.jpg" class="imgservice" alt="Process">
+                    <h5>Select Files</h5>
+                </div>
+                <div class="col-md-4 col-sm-4 wow bounceInUp">
+                    <img src="images/edit.jpg" class="imgservice" alt="Process">
+                    <h5>Edit and convert file</h5>
+                </div>
+                <div class="col-md-4 col-sm-4 wow bounceInUp">
+                    <img src="images/down.jpg" class="imgservice" alt="Process">
+                    <h5>Download file</h5>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+</main>
+<div id="myModal" class="modal fade" role="dialog">
+    <div class="modal-dialog modal-lg">
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-body">
+                {{ Form::open(array('method'=>'post','class'=> '','url' => '/comment/add', 'id'=>'comment')) }}
+                <div class="file-upload">
+                    <div class="image-upload-wrap">
+                        <input class="file-upload-input" type='file' onchange="readURL(this);" accept=".xlsx,.xls,image/*,.doc/*,.docx/*,.ppt,.pptx,.txt,.pdf" />
+                        <div class="drag-text">
+                            <h3>Drag and drop a file or select add Image</h3>
+                            <p>OR</p>
+                        </div>
+                        <div class="select_btn">
+                            <button class="file-upload-btn" type="button" onclick="$('.file-upload-input').trigger( 'click' )">Select File</button>
+                        </div>
+                    </div>
+                    <div class="file-upload-content">
+                        <img class="file-upload-image" src="#" alt="your image" />
+                        <div class="image-title-wrap">
+                            <button type="button" onclick="removeUpload()" class="remove-image">Remove <span class="image-title">Uploaded Image</span></button>
+                        </div>
+                    </div>
+                </div>
+                <div class="drive_upload text-center">
+                    <p>OR</p>
+                    <h3>Upload File From</h3>
+                    <ul class="list-inline list-unstyled drive_img">
+                        <li>
+                            <a href="#">
+                                <img src="images/Box_icon.png" alt="Drive Icon" title="Drop Box">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="images/Drive_icon.png" alt="Drive Icon" title="Google Drive">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="images/cloud_icon.png" alt="Drive Icon" title="Cloud">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                {{ Form::close() }}
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
