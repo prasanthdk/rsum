e13c504df3bb418571ea6fd71cb1d96d7ec81517
Meenaharini <meenakumari.r@pickzy.com>
2018-07-31 18:21:18 +0530

    Login UI

--- pdfCloud/resources/views/auth/login.blade.php
+++ pdfCloud/resources/views/auth/login.blade.php
@@ -1,7 +1,68 @@
 @extends('layouts.frontend')
 
 @section('content')
-<div class="container">
+<section class="login-page">
+      <div class="container">
+        <div class="rowlogin-content">
+                    <div class="col-md-12 col-sm-12">
+                        <div class="login-content-inner">
+                            <div class="login-title">
+                                <h3 class="text-center">My Account</h3>
+                            </div>
+                            <form class="form-horizontal" id="loginForm" method="POST" action="{{ route('login') }}">
+                        {{ csrf_field() }}
+                                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                    <label for="email" class="emailaddrs">Email Address</label>
+                                    <input type="email" name="email" id="email" class="form-control" value="{{ old('email') }}" autofocus>
+                                    @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                                </div>
+                                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                    
+                                            <label for="password"  class="emailaddrs">Password</label>
+                                             <input type="password" name="password" id="password" class="form-control" >
+                                             @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                                       <a href="{{ route('password.request') }}" style="text-align: right;"><p class="text-right pass">Forgot your password?</p></a>
+                                    </div>
+                                
+                                    
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-md-12 col-sm-12">
+                                            <div class="checkbox checkbox-primary">
+                                                
+                                                    
+                                               
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary btn-block logbtn">Login</button>
+                                </div>
+                                <div class="form-group">
+                                    <div class="row text-center">
+                                        <div class="col-md-12 col-sm-12">
+                                            <label>No account ?<span><a href=""> Signup</a></span></label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+          
+        </div>
+      </div>
+      <div class="clearfix"></div>
+    </section>
+<!-- <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -65,5 +126,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 @endsection
--- pdfCloud/resources/views/auth/register.blade.php
+++ pdfCloud/resources/views/auth/register.blade.php
@@ -1,7 +1,91 @@
 @extends('layouts.frontend')
 
 @section('content')
-<div class="container">
+ <section class="login-page">
+      <div class="container">
+
+        <!--sign-->
+
+<div class="row register-form">
+                    <div class="col-sm-5">
+
+              <form class="form-horizontal r-form" id="registerForm" method="POST" action="{{ route('register') }}">
+                        {{ csrf_field() }}
+              <h3>Create An Account</h3>
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+
+                            <input id="email" type="email" class="form-control r-form-first-name" name="email" value="{{ old('email') }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                          </div>
+                          <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+
+                            <input id="password" type="password" class="form-control r-form-last-name" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+
+                            <p class="text-left accs">Already have an account ?</p>
+
+                            <a href="" style="text-align: right;"><p class="text-left accs logs">Login</p></a>
+                          </div>
+                      <div class="form-group btncnt">    
+                    <button type="submit" class="btn countinue" id="pdf_register_btn">Continue</button>
+                  </div>
+
+            </form>
+            
+                    </div>
+
+            <div class="col-sm-1 borderright">
+            </div>
+            <div class="col-sm-1">
+            </div>
+
+                    <div class="col-sm-5 forms-right-icons">
+              <h3>PDF Made Easy</h3>
+            <div class="row">
+              <div class="col-sm-2 icon"><img src="images/online.png"></div>
+              <div class="col-sm-10 instants">
+                <p>Edit PDF documents online instantly. </p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-2 icon"><img src="images/edit.png"></div>
+              <div class="col-sm-10 instants">
+                <p>PDFCloud is your all in one solution for editing and converting PDF documents</p>
+              </div>
+            </div> 
+            <div class="row">
+              <div class="col-sm-2 icon"><img src="images/storage.png"></div>
+              <div class="col-sm-10 instants">
+                <p>Now offering unlimited cloud storage for all users.</p>
+              </div>
+            </div>
+                        <div class="row">
+                            <div class="col-sm-2 icon"><img src="images/click.png"></div>
+                            <div class="col-sm-10 instants">
+                                <p>Click Upload Document to get started today.</p>
+                            </div>
+                        </div>
+                        
+                    </div>
+<p class="consent">By continuing, you confirm that you have read and consent to our Terms of Use and Privacy Policy.</p>
+                </div>
+
+        <!--end-->
+        
+      </div>
+      <div class="clearfix"></div>
+    </section>
+<!-- <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -13,16 +97,6 @@
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                             <label for="name" class="col-md-4 control-label">Name</label>
-
-                            <!-- <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div> -->
                         </div>
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
@@ -53,14 +127,6 @@
                             </div>
                         </div>
 
-                        <!-- <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div> -->
-
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary" id="pdf_register_btn">
@@ -73,5 +139,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 @endsection
