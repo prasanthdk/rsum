ebdf6a4e12a95d592fdc56a0efbbc24dd33599a0
Nidheesh <nidheesh.tk@pickzy.com>
2018-08-03 15:13:43 +0530

    migration file

---
+++ pdfCloud/database/migrations/2018_08_03_093854_add_field_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('activation_code')->after('remember_token')->comment('user mail verification token');
+            $table->enum('status',[2,1])->after('activation_code')->comment('1-active,2-inactive');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('activation_code');
+            $table->dropColumn('status');
+        });
+    }
+}
